6794d6ffe35a78bcfae0d0589a4aa7711151df7d
linkneo-cmd <81950756+linkneo-cmd@users.noreply.github.com>
2023-12-02 23:55:58 +0800

    Add files via upload
    可上下拖动的商城页面

---
+++ MyApplication/entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/pages/Index.js
@@ -0,0 +1,82 @@
+"use strict";
+class Index extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1) {
+        super(parent, __localStorage, elmtId);
+        this.__message = new ObservedPropertySimplePU('Hello World', this, "message");
+        this.setInitiallyProvidedValue(params);
+    }
+    setInitiallyProvidedValue(params) {
+        if (params.message !== undefined) {
+            this.message = params.message;
+        }
+    }
+    updateStateVars(params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+        this.__message.purgeDependencyOnElmtId(rmElmtId);
+    }
+    aboutToBeDeleted() {
+        this.__message.aboutToBeDeleted();
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    get message() {
+        return this.__message.get();
+    }
+    set message(newValue) {
+        this.__message.set(newValue);
+    }
+    initialRender() {
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Row.create();
+            Row.height('100%');
+            if (!isInitialRender) {
+                Row.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Column.create();
+            Column.width('100%');
+            if (!isInitialRender) {
+                Column.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Text.create(this.message);
+            Text.fontSize(50);
+            Text.fontWeight(FontWeight.Bold);
+            Text.fontColor('#36D');
+            Text.onClick(() => {
+                this.message = 'Hello ArkTS!';
+            });
+            if (!isInitialRender) {
+                Text.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        Text.pop();
+        this.observeComponentCreation((elmtId, isInitialRender) => {
+            ViewStackProcessor.StartGetAccessRecordingFor(elmtId);
+            Image.create('https://res.vmallres.com/pimages//uomcdn/CN/pms/202305/gbom/6942103102790/group//428_428_B5CF48BC9E98F629F44ED8DF4F70EB86.png');
+            Image.width(250);
+            if (!isInitialRender) {
+                Image.pop();
+            }
+            ViewStackProcessor.StopGetAccessRecording();
+        });
+        Column.pop();
+        Row.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+}
+ViewStackProcessor.StartGetAccessRecordingFor(ViewStackProcessor.AllocateNewElmetIdForNextComponent());
+loadDocument(new Index(undefined, {}));
+ViewStackProcessor.StopGetAccessRecording();
+//# sourceMappingURL=Index.js.map
